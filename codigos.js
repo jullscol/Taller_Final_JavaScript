109f69acb1ca5a7612a21d5a95e04ed562a28853
Julian Arbelaez <arbelaezjul2012@gmail.com>
2022-03-08 23:09:23 -0500

    Punto2
    Primera versión punto2

---
+++ codigos.js
@@ -0,0 +1,11 @@
+const cambiarColor= () => {
+
+	let element = document.querySelector(".first");
+	element.classList.add("estilo1");
+}
+ 
+ const removerEstilos = () => {
+
+	let element = document.querySelector(".first");
+	element.classList.remove("estilo1");
+} 
