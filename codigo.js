90a0bd39a367dc4455307f2b4bcdf785bac00f76
Julian Arbelaez <arbelaezjul2012@gmail.com>
2022-03-08 22:26:51 -0500

    Punto1
    Primer versión punto1

---
+++ codigo.js
@@ -0,0 +1,32 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("form").addEventListener('submit', validarFormulario); 
+});
+
+function validarFormulario(evento) {
+  evento.preventDefault();
+  var nombre = document.getElementById('nombre').value;
+  console.log(nombre);
+  if(nombre.length == 0) {
+    alert('No ha escrito el nombre');
+     
+     return;
+     console.log(nombre);
+
+  }
+  var email = document.getElementById('email').value;
+  if(email.length == 0) {
+    alert('No ha escrito el email');
+ 
+    return;
+  }
+  var asunto = document.getElementById('asunto').value;
+   if(asunto.length == 0) {
+    alert('No ha escrito el asunto');
+
+    return;
+  }
+
+    this.submit();
+}
+
